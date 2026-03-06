710a398d046add64ad92b9d841c28c18b3193bfe
Dwig Yadav <dwig236434@gmail.com>
2026-03-06 15:45:41 +0530

    Refactor config.py by removing comments and whitespace

--- project37/config.py
+++ project37/config.py
@@ -6,13 +6,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'csi_detection.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Path to your trained keras model
+   
     MODEL_PATH = os.environ.get('MODEL_PATH', os.path.join(BASE_DIR, 'best_csi_model_ml.pkl'))
 
-    # Dashboard auto-refresh interval (seconds)
     REFRESH_INTERVAL = 5
 
-    # CSI constants (must match training)
+   
     TIME_STEPS   = 500
     SUBCARRIERS  = 52
     NUM_CLASSES  = 5
@@ -25,7 +24,7 @@ class Config:
         4: 'Cow',
     }
 
-    # Classes that trigger an alert (anything except background)
+   
     ALERT_CLASSES = [1, 2, 3, 4]
 
     CLASS_COLORS = {
